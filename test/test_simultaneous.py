Fixed loss calculation + add more testing

test/test_simultaneous.py
@@ -7,7 +7,125 @@ import xnmt
 import xnmt.modules.nn as nn
 
 
-class TestSimultaneousTranslation(unittest.TestCase):
+class TestSimultaneousTranslationRRWW(unittest.TestCase):
+
+  def setUp(self):
+    random.seed(7)
+    numpy.random.seed(7)
+    # Seeding
+    layer_dim = 32
+    xnmt.internal.events.clear()
+    xnmt.internal.param_collections.ParamManager.init_param_col()
+
+    self.src_reader = xnmt.modules.input_readers.SimultTextReader(
+      text_reader=xnmt.modules.input_readers.PlainTextReader(vocab=xnmt.Vocab(vocab_file="examples/data/head.ja.vocab")),
+      action_reader=xnmt.modules.input_readers.PlainTextReader(vocab=xnmt.structs.vocabs.SimultActionVocab())
+    )
+    self.trg_reader = xnmt.modules.input_readers.PlainTextReader(vocab=xnmt.Vocab(vocab_file="examples/data/head.en.vocab"))
+    self.layer_dim = layer_dim
+    self.src_data = list(self.src_reader.read_sents(["examples/data/head.ja", "examples/data/simult/head.jaen.rrww.actions"]))
+    self.trg_data = list(self.trg_reader.read_sents("examples/data/head.en"))
+    self.input_vocab_size = len(self.src_reader.vocab.i2w)
+    self.output_vocab_size = len(self.trg_reader.vocab.i2w)
+    self.loss_calculator = xnmt.train.MLELoss()
+
+    self.model = xnmt.networks.SimultSeq2Seq(
+      src_reader=self.src_reader,
+      trg_reader=self.trg_reader,
+      encoder=nn.SeqEncoder(
+        embedder=nn.LookupEmbedder(emb_dim=layer_dim, vocab=self.src_reader.vocab),
+        seq_transducer=nn.UniLSTMSeqTransducer(input_dim=layer_dim, hidden_dim=layer_dim)),
+      decoder=nn.ArbLenDecoder(
+        input_dim=layer_dim,
+        attender=nn.MlpAttender(input_dim=layer_dim, state_dim=layer_dim, hidden_dim=layer_dim),
+        embedder=nn.LookupEmbedder(emb_dim=layer_dim, vocab=self.trg_reader.vocab),
+        rnn=nn.UniLSTMSeqTransducer(input_dim=layer_dim,
+                                    hidden_dim=layer_dim,
+                                    decoder_input_dim=layer_dim,
+                                    yaml_path=xnmt.Path("model.decoder.rnn")),
+        transform=nn.NonLinear(input_dim=layer_dim*2, output_dim=layer_dim),
+        scorer=nn.Softmax(input_dim=layer_dim, vocab=self.trg_reader.vocab),
+        bridge=nn.NoBridge(dec_dim=layer_dim)),
+      policy_agent=xnmt.rl.agents.SimultPolicyAgent(
+        oracle_in_train=True,
+        oracle_in_test=True,
+        default_layer_dim=layer_dim
+      )
+    )
+
+    self.seq2seq = xnmt.networks.Seq2Seq(
+      src_reader=self.src_reader,
+      trg_reader=self.trg_reader,
+      encoder=self.model.encoder,
+      decoder=self.model.decoder
+    )
+
+    my_batcher = xnmt.structs.batchers.TrgBatcher(batch_size=3)
+    self.src, self.trg = my_batcher.pack(self.src_data, self.trg_data)
+    dy.renew_cg(immediate_compute=True, check_validity=True)
+
+  def test_train_nll(self):
+    xnmt.event_trigger.set_train(True)
+    mle_loss = xnmt.train.MLELoss()
+    mle_loss.calc_loss(self.model, self.src[0], self.trg[0])
+    xnmt.event_trigger.set_train(False)
+    result = self.model.generate(xnmt.mark_as_batch([self.src_data[0]]), xnmt.inferences.GreedySearch())
+    self.assertNotEqual(result, self.trg_data[0].sent_len())
+    result = self.model.generate(xnmt.mark_as_batch([self.src_data[0]]), xnmt.inferences.BeamSearch())
+    self.assertNotEqual(result, self.trg_data[0].sent_len())
+    inference = xnmt.inferences.AutoRegressiveInference(src_file=["examples/data/head.ja",
+                                                                  "examples/data/simult/head.jaen.actions"],
+                                                        ref_file="examples/data/head.en",
+                                                        trg_file="test/output/hyp",
+                                                        search_strategy=xnmt.inferences.GreedySearch(),
+                                                        reporter=None)
+    inference.perform_inference(self.model)
+
+
+  def test_same_loss_batch_single(self):
+    xnmt.event_trigger.set_train(True)
+    self.model.policy_agent.policy_network = None
+    self.model.train_pol_mle = False
+    mle_loss = xnmt.train.MLELoss()
+    for src, trg in zip(self.src, self.trg):
+      loss, loss_stat = mle_loss.calc_loss(self.model, src, trg).compute()
+      losses = []
+      for s, t in zip(src, trg):
+        loss_i, _ = mle_loss.calc_loss(self.model,
+                                       xnmt.mark_as_batch([s.get_unpadded_sent()]),
+                                       xnmt.mark_as_batch([t.get_unpadded_sent()])).compute()
+        losses.append(loss_i)
+
+      self.assertAlmostEqual(dy.sum_batches(loss).scalar_value(), dy.esum(losses).scalar_value(), places=8)
+
+
+  def test_loss_equal_to_seq2seq(self):
+    xnmt.event_trigger.set_train(True)
+    self.model.policy_agent.policy_network = None
+    self.model.train_pol_mle = False
+    mle_loss = xnmt.train.MLELoss()
+    for src, trg in zip(self.src, self.trg):
+      loss, loss_stat = mle_loss.calc_loss(self.model, src, trg).compute()
+      sloss, sloss_stat = mle_loss.calc_loss(self.seq2seq, src, trg).compute()
+      numpy.testing.assert_array_almost_equal(loss.npvalue(), sloss.npvalue(), decimal=8)
+
+  def test_same_loss_batch_single_pol(self):
+    xnmt.event_trigger.set_train(True)
+    self.model.train_nmt_mle = False
+    mle_loss = xnmt.train.MLELoss()
+    for src, trg in zip(self.src, self.trg):
+      loss, loss_stat = mle_loss.calc_loss(self.model, src, trg).compute()
+      losses = []
+      for s, t in zip(src, trg):
+        loss_i, _ = mle_loss.calc_loss(self.model,
+                                       xnmt.mark_as_batch([s.get_unpadded_sent()]),
+                                       xnmt.mark_as_batch([t.get_unpadded_sent()])).compute()
+        losses.append(loss_i)
+
+      self.assertAlmostEqual(dy.sum_batches(loss).scalar_value(), dy.esum(losses).scalar_value(), places=8)
+
+
+class TestSimultaneousTranslationOracle(unittest.TestCase):
 
   def setUp(self):
     random.seed(7)
@@ -53,6 +171,13 @@ class TestSimultaneousTranslation(unittest.TestCase):
       )
     )
 
+    self.seq2seq = xnmt.networks.Seq2Seq(
+      src_reader=self.src_reader,
+      trg_reader=self.trg_reader,
+      encoder=self.model.encoder,
+      decoder=self.model.decoder
+    )
+
     my_batcher = xnmt.structs.batchers.TrgBatcher(batch_size=3)
     self.src, self.trg = my_batcher.pack(self.src_data, self.trg_data)
     dy.renew_cg(immediate_compute=True, check_validity=True)
@@ -63,9 +188,9 @@ class TestSimultaneousTranslation(unittest.TestCase):
     mle_loss.calc_loss(self.model, self.src[0], self.trg[0])
     xnmt.event_trigger.set_train(False)
     result = self.model.generate(xnmt.mark_as_batch([self.src_data[0]]), xnmt.inferences.GreedySearch())
-    self.assertNotEqual(len(result), 0)
+    self.assertEqual(len(result), self.trg_data[0].sent_len())
     result = self.model.generate(xnmt.mark_as_batch([self.src_data[0]]), xnmt.inferences.BeamSearch())
-    self.assertNotEqual(len(result), 0)
+    self.assertEqual(len(result), self.trg_data[0].sent_len())
     inference = xnmt.inferences.AutoRegressiveInference(src_file=["examples/data/head.ja",
                                                                   "examples/data/simult/head.jaen.actions"],
                                                         ref_file="examples/data/head.en",
@@ -74,7 +199,7 @@ class TestSimultaneousTranslation(unittest.TestCase):
                                                         reporter=None)
     inference.perform_inference(self.model)
 
-  
+
   def test_same_loss_batch_single(self):
     xnmt.event_trigger.set_train(True)
     self.model.policy_agent.policy_network = None
@@ -88,9 +213,9 @@ class TestSimultaneousTranslation(unittest.TestCase):
                                        xnmt.mark_as_batch([s.get_unpadded_sent()]),
                                        xnmt.mark_as_batch([t.get_unpadded_sent()])).compute()
         losses.append(loss_i)
-      
-      self.assertAlmostEqual(dy.sum_batches(loss).scalar_value(), dy.esum(losses).scalar_value(), places=1)
-    
+
+      self.assertEqual(dy.sum_batches(loss).scalar_value(), dy.esum(losses).scalar_value())
+
   def test_same_loss_batch_single_pol(self):
     xnmt.event_trigger.set_train(True)
     self.model.train_nmt_mle = False
@@ -103,8 +228,8 @@ class TestSimultaneousTranslation(unittest.TestCase):
                                        xnmt.mark_as_batch([s.get_unpadded_sent()]),
                                        xnmt.mark_as_batch([t.get_unpadded_sent()])).compute()
         losses.append(loss_i)
-      
-      self.assertAlmostEqual(dy.sum_batches(loss).scalar_value(), dy.esum(losses).scalar_value(), places=0)
+
+      self.assertAlmostEqual(dy.sum_batches(loss).scalar_value(), dy.esum(losses).scalar_value(), places=4)
 
 
   def test_recurrent_agent(self):

xnmt/networks/simult_seq2seq.py
@@ -33,14 +33,17 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
 
   def initial_state(self, src: xnmt.Batch, force_oracle=False) -> agents.SimultSeqLenUniDirectionalState:
     oracle_batch = None
+    trg_count = None
     if type(src[0]) == xnmt.structs.sentences.OracleSentence:
       oracle = [src[i].oracle for i in range(src.batch_size())]
+      trg_count = [sum([1 for x in src[i].oracle if x == agents.SimultPolicyAgent.WRITE]) for i in range(src.batch_size())]
       oracle_batch = xnmt.structs.batchers.pad(oracle)
 
     encoding = self.encoder.encode(src)
     encoder_seqs = encoding.encode_seq
     return agents.SimultSeqLenUniDirectionalState(
-      oracle_batch=oracle_batch, src=src, full_encodings=encoder_seqs, network_state=self.policy_agent.initial_state(src)
+      oracle_batch=oracle_batch, src=src, full_encodings=encoder_seqs, network_state=self.policy_agent.initial_state(src),
+      trg_counts=trg_count
     )
 
   def add_input(self, prev_word: xnmt.Batch, state: models.UniDirectionalState) -> agents.SimultSeqLenUniDirectionalState:
@@ -49,7 +52,6 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
 
     while not self.finish_generating(prev_word, state):
       search_action, network_state = self.policy_agent.next_action(state)
-
       if search_action.action_id == agents.SimultPolicyAgent.READ:
         state = self._perform_read(state, search_action, network_state)
       elif search_action.action_id == agents.SimultPolicyAgent.WRITE:
@@ -62,7 +64,16 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
 
   def calc_nll(self, src: xnmt.Batch, trg: xnmt.Batch):
     state = self.initial_state(src, force_oracle=True)
-    pad_token = xnmt.structs.vocabs.SimultActionVocab.PAD
+    word_pad = xnmt.structs.vocabs.Vocab.PAD
+
+    if hasattr(state, "oracle_batch"):
+      for s, t, o in zip(src, trg, state.oracle_batch):
+        s = s.get_unpadded_sent()
+        t = t.get_unpadded_sent()
+        o = o.get_unpadded_sent()
+        assert o.sent_len() == s.sent_len() + t.sent_len(), \
+          "Expecting {} + {} but got {} for oracle len.\nSRC: {}\nTRG: {}\nORC: {}".format(
+            s.sent_len(), t.sent_len(), o.sent_len(), str(s), str(t), str(o))
 
     mle_loss = []
     pol_loss = []
@@ -77,17 +88,24 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
         num_reads = state.num_reads
 
       if agents.SimultPolicyAgent.WRITE in action_set:
-        prev_word = [trg[i][min(state.num_writes[i]-1, trg.sent_len()-1)] \
-                       if state.num_writes[i] > 0 else pad_token for i in range(trg.batch_size())]
-        ref_word = [trg[i][min(state.num_writes[i], trg.sent_len()-1)] \
-                      if state.num_writes[i] < trg[i].sent_len() else pad_token for i in range(trg.batch_size())]
         write_flag = np.zeros((trg.batch_size(), 1), dtype=int)
         write_flag[search_action.action_id == agents.SimultPolicyAgent.WRITE] = 1
-        input_mask = np.array([[1 if word == pad_token else 0 for word in prev_word]])
-        input_mask = np.bitwise_or(input_mask.transpose(), 1-write_flag)
-        prev_word = xnmt.mark_as_batch(data=prev_word, mask=xnmt.Mask(input_mask))
 
-        new_state =  self._perform_write(state, search_action, prev_word, network_state, write_flag.transpose()[0])
+        prev_word = [trg[i][state.num_writes[i]-1] \
+                       if write_flag[i] > 0 and state.num_writes[i] > 0 \
+                       else word_pad for i in range(trg.batch_size())]
+        prev_mask = np.array([[1 if word == word_pad else 0 for word in prev_word]])
+        prev_word = xnmt.mark_as_batch(data=prev_word, mask=xnmt.Mask(prev_mask.transpose()))
+
+
+        ref_word = [trg[i][state.num_writes[i]] \
+                      if write_flag[i] else word_pad for i in range(trg.batch_size())]
+        ref_mask = np.array([[1 if word == word_pad else 0 for word in ref_word]])
+        ref_word = xnmt.mark_as_batch(data=ref_word, mask=xnmt.Mask(ref_mask.transpose()))
+
+        new_state =  self._perform_write(state, search_action, prev_word, network_state, np.asarray(1-ref_mask[0], int))
+        #if state.decoder_state is not None:
+        #  print(state.decoder_state.attender_state.read_mask.np_arr)
         num_writes = new_state.num_writes
         decoder_state = new_state.decoder_state
 
@@ -96,8 +114,13 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
           loss = self.decoder.calc_loss(decoder_state, ref_word)
           if ref_word.mask is not None:
             loss = ref_word.mask.cmult_by_timestep_expr(loss, 0, True)
-            
+
           mle_loss.append(loss)
+        # BEGIN(DEBUG)
+#        print("{} {} {} {} {} {}".format(search_action.action_id, mle_loss[-1].npvalue(), prev_word,
+#                                      prev_word.mask.np_arr.transpose(), ref_word,
+#                                      ref_word.mask.np_arr.transpose()))
+        # END(DEBUG)
       else:
         num_writes = state.num_writes
         decoder_state = state.decoder_state
@@ -105,15 +128,17 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
         write_flag[search_action.action_id == agents.SimultPolicyAgent.WRITE] = 1
 
       if self.train_pol_mle:
+        action_pad = xnmt.structs.vocabs.SimultActionVocab.PAD
         search_action = search_action.action_id
         oracle_action = [oracle[state.timestep] for oracle in state.oracle_batch]
-        mask = xnmt.Mask(np.array([[1 if ref == pad_token else 0 for ref in oracle_action]]).transpose())
+        mask = xnmt.Mask(np.array([[1 if ref == action_pad else 0 for ref in oracle_action]]).transpose())
         oracle_batch = xnmt.mark_as_batch(oracle_action, mask)
         loss = self.policy_agent.calc_loss(network_state, oracle_batch)
+
         if oracle_batch.mask is not None:
           loss = oracle_batch.mask.cmult_by_timestep_expr(loss, 0, True)
         pol_loss.append(loss)
-    
+
       state = agents.SimultSeqLenUniDirectionalState(
         src=state.src,
         full_encodings=state.full_encodings,
@@ -126,8 +151,10 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
         network_state=network_state,
         parent=state,
         force_oracle=state.force_oracle,
-        timestep=state.timestep+1
+        timestep=state.timestep+1,
+        trg_counts=state.trg_counts
       )
+
     total_loss = {}
     if mle_loss:
       total_loss["p(e|f)"] = xnmt.LossExpr(dy.esum(mle_loss), units=state.num_writes)
@@ -138,8 +165,8 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
 
   def finish_generating(self, output: Any, dec_state: agents.SimultSeqLenUniDirectionalState):
     if dec_state.force_oracle or \
-        xnmt.is_train() and self.policy_agent.oracle_in_train or \
-        (not xnmt.is_train()) and  self.policy_agent.oracle_in_test:
+        (xnmt.is_train() and self.policy_agent.oracle_in_train) or \
+        (not xnmt.is_train() and  self.policy_agent.oracle_in_test):
       return self.policy_agent.finish_generating(dec_state)
     return super().finish_generating(output, dec_state)
 
@@ -162,7 +189,8 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
       oracle_batch=state.oracle_batch,
       timestep=state.timestep+1,
       force_oracle=state.force_oracle,
-      parent=state
+      parent=state,
+      trg_counts=state.trg_counts
     )
 
   def _perform_write(self,
@@ -170,7 +198,7 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
                      search_action: models.SearchAction,
                      prev_word: xnmt.Batch,
                      network_state: models.PolicyAgentState,
-                     write_flag: np.ndarray = np.ndarray([1])) -> agents.SimultSeqLenUniDirectionalState:
+                     write_flag: np.ndarray = np.asarray([1], int)) -> agents.SimultSeqLenUniDirectionalState:
     if state.decoder_state is None:
       decoder_state = self.decoder.initial_state(models.EncoderState(state.full_encodings, None), state.src)
     else:
@@ -210,4 +238,5 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
       force_oracle=state.force_oracle,
       timestep=state.timestep+1,
       parent=state,
+      trg_counts=state.trg_counts
     )

xnmt/structs/batch.py
@@ -65,7 +65,7 @@ class Mask(object):
         return expr
       mask_exp = dy.inputTensor(self.np_arr[:,timestep:timestep+1].transpose(), batched=True)
     return dy.cmult(expr, mask_exp)
-  
+
   def cmult_to_tensor_expr(self, tensor_exp: dy.Expression, inverse=True):
     inp = self.np_arr if not inverse else 1-self.np_arr
     inp_expr = dy.inputTensor(np.expand_dims(inp.transpose(),axis=0), batched=True)