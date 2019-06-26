Fixed reinf loss calculation

xnmt/networks/simult_seq2seq.py
@@ -31,7 +31,11 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
                default_layer_dim: int = xnmt.default_layer_dim,
                permute: Optional[float] = 0,
                z_normalization: Optional[bool] = False,
-               bleu_score_only_reward: Optional[bool] = False):
+               bleu_score_only_reward: Optional[bool] = False,
+               train_src_predictor: Optional[bool] = True,
+               src_predictor_rnn: Optional[models.UniDiSeqTransducer] = None,
+               src_predictor_softmax: Optional[models.Scorer] = None
+               ):
     super().__init__(src_reader=src_reader, trg_reader=trg_reader, encoder=encoder, decoder=decoder)
     self.policy_agent = policy_agent
     self.train_nmt_mle = train_nmt_mle
@@ -44,6 +48,10 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
     self.permute = permute
     self.z_normalization = z_normalization
     self.bleu_score_only_reward = bleu_score_only_reward
+
+    self.src_predictor_rnn = src_predictor_rnn
+    self.src_predictor_softmax = src_predictor_softmax
+    self.train_src_predictor = train_src_predictor
 
     if isinstance(decoder, nn.ArbLenDecoder):
       if not isinstance(decoder.bridge, nn.ZeroBridge):
@@ -197,6 +205,10 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
         trg_counts=state.trg_counts
       )
     # END: Loop
+    src_predictor_loss = []
+    if self.train_src_predictor:
+      self.src_predictor_rnn.transduce()
+
     ### Calculate Total Loss
     total_loss = {}
     if mle_loss:
@@ -242,15 +254,6 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
 
         ### Next Actions
         search_action, network_state = self.policy_agent.next_action(state, is_sample=True, force_oracle=force_oracle)
-        done_mask = dy.inputTensor([0 if done[i] else 1 for i in range(batch_size)], batched=True)
-        search_action.action_id[done] = xnmt.structs.vocabs.SimultActionVocab.PAD
-        log_ll.append(dy.cmult(search_action.log_likelihood, done_mask))
-        actions.append(search_action.action_id)
-
-        ### Baseline
-        bs_inp = network_state.output() or dy.zeros(*network_state.output().dim())
-        baseline_inp.append(self.baseline_network.transform(dy.nobackprop(bs_inp)))
-        baseline_flg.append(done_mask)
 
         ### PERFORM READING + WRITING
         action_set = set(search_action.action_id)
@@ -272,15 +275,27 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
           prev_word = xnmt.mark_as_batch(prev_word, write_mask)
           write_state = self._perform_write(state, search_action, prev_word, write_flag)
 
-          search_action = self.best_k(write_state, 1, True)
+          write_action = self.best_k(write_state, 1, True)
           for i in range(batch_size):
             if write_flag[i]:
-              word = search_action[0].action_id[i]
+              word = write_action[0].action_id[i]
               words[i].append(word)
-              done[i] = len(words[i]) - 1 >= max_len or word == self.decoder.eog_symbol
-
+              done[i] = done[i] or \
+                        len(words[i]) - 1 >= max_len or \
+                        word == self.decoder.eog_symbol
+            done[i] = done[i] or search_action.action_id[i] == 2
         else:
           write_state = state
+
+        done_mask = dy.inputTensor([0 if done[i] else 1 for i in range(batch_size)], batched=True)
+        search_action.action_id[done] = xnmt.structs.vocabs.SimultActionVocab.PAD
+        log_ll.append(dy.cmult(search_action.log_likelihood, done_mask))
+        actions.append(search_action.action_id)
+
+        ### Baseline
+        bs_inp = network_state.output() or dy.zeros(*network_state.output().dim())
+        baseline_inp.append(self.baseline_network.transform(dy.nobackprop(bs_inp)))
+        baseline_flg.append(done_mask)
 
         ### Next State ###
         state = agents.SimultSeqLenUniDirectionalState(
@@ -329,6 +344,7 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
       ### Reward Discount ###
       baseline = dy.concatenate(baseline_inp, d=0)
       flags = dy.concatenate(baseline_flg, d=0)
+      baseline = dy.cmult(baseline, flags)
       before_reward = dy.cmult(reward, flags)
       if not self.no_baseline:
         reward = before_reward - baseline
@@ -336,17 +352,18 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
         reward = before_reward
 
       ### Variance Reduction ###
-#      if self.z_normalization:
-#        r_mean = dy.mean_dim(reward, d=[0], b=True)
-#        r_std = dy.std_dim(reward, d=[0], b=True)
-#        reward = dy.cdiv((reward - r_mean), r_std + 1e-6)
+      if self.z_normalization:
+        r_mean = dy.mean_dim(reward, d=[0], b=False)
+        r_std = dy.std_dim(reward, d=[0], b=False)
+        reward = dy.cdiv((reward - r_mean), r_std + 1e-6)
 
       ### calculate loss ###
 #      reward = dy.nobackprop(reward)
       log_ll = dy.concatenate(log_ll, d=0)
-      rf_loss = dy.sum_elems(dy.cmult(reward, log_ll))
+      rf_loss = dy.cmult(reward, log_ll)
+      rf_loss = dy.cmult(rf_loss, flags)
       rf_units = [len(x) for (x) in words]
-      reinf_losses.append(xnmt.LossExpr(rf_loss, rf_units))
+      reinf_losses.append(xnmt.LossExpr(dy.sum_elems(rf_loss), rf_units))
 
       baseline_units = np.sum(flags.npvalue(), axis=0)
       b_units = [1 for _ in range(flags.dim()[1])]
@@ -356,15 +373,43 @@ class SimultSeq2Seq(base.Seq2Seq, xnmt.Serializable):
         basel_losses.append(xnmt.LossExpr(dy.sum_elems(baseline_loss), b_units))
 
       if xnmt.is_train():
-        try:
+#        try:
           print("[{}] BLEU: {:.5f}, LL: {:.5f}, RW: {:.5f}".format(
             1 if force_oracle else 0,
             np.mean(tr_bleus),
             np.mean(dy.cdiv(dy.sum_elems(log_ll), dy.inputTensor(baseline_units, batched=True)).value()),
             np.mean(dy.sum_elems(reward).value())
           ))
-        except:
-          pass
+
+          for i in range(batch_size):
+            a_bs = dy.pick_batch_elem(baseline, i).npvalue()
+            a_bs_loss = dy.pick_batch_elem(baseline_loss, i).npvalue()
+            a_log_ll = dy.pick_batch_elem(log_ll, i).npvalue()
+            a_bef_rw = dy.pick_batch_elem(before_reward, i).npvalue()
+            a_dis_rw = dy.pick_batch_elem(reward, i).npvalue()
+            a_r_loss = dy.pick_batch_elem(rf_loss, i).npvalue()
+            a_flags = dy.pick_batch_elem(flags, i).npvalue()
+            a = actions[i]
+
+            print("FL  A   LOG_LL    RW     BS   RW-BS   BS_L   RF_L")
+            for t in range(len(a)):
+              if a[t] != 2:
+                print("[{}] {} {: .5f} {: .3f} {: .3f} {: .3f} {: .3f} {: .3f}".format(
+                  int(a_flags[t]),
+                  a[t], a_log_ll[t], a_bef_rw[t], a_bs[t], a_dis_rw[t], a_bs_loss[t],
+                  a_r_loss[t]
+                ))
+              else:
+                print("[{}] {} -inf     {: .3f} {: .3f} {: .3f} {: .3f} {: .3f}".format(
+                  int(a_flags[t]),
+                  a[t], a_bef_rw[t], a_bs[t], a_dis_rw[t], a_bs_loss[t],
+                  a_r_loss[t]
+                ))
+            print()
+
+
+#        except:
+#          pass
     # END LOOP: Sample
 
     rf_loss = functools.reduce(lambda x, y: x+y, reinf_losses)